f95ddbb961f5e8e081ac1ee44651d8a26ecfa346
SamukaCode <99834471+SamukaCode@users.noreply.github.com>
2023-02-10 09:55:48 -0300

    Add files via upload

---
+++ JavaScript/Variáveis/conceito.js
@@ -0,0 +1,28 @@
+// escopo de var
+var nome = 'Samuka'
+
+if(nome != null) {
+    var sobrenome = 'Camargo';
+    console.timeLog(nome + ' ' + sobrenome);
+}
+
+console.log(sobrenome) /* Acessível fora do if*/
+
+// let
+
+let universidade = 'Unicamp'
+
+if(nome != null) {
+    let colegio = 'Cotil';
+    console.log(Unicamp + '/' + colegio);
+}
+
+console.log(colegio) /* Não é acessível fora do if*/
+
+// const
+
+const IDADE = 18;
+// idade = 21; - ERRADO, não pode mudar 
+console.log(IDADE);
+
+// É uma boa prática de programação declarar const em MAIÚSCULA.
