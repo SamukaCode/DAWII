d5261592357f374c38fe94043d66599dd0547454
SamukaCode <samuelpcamargo2005@gmail.com>
2023-06-12 21:50:41 -0300

    .

--- php/bd/conexao.php
+++ php/bd/conexao.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $db = 'mysql:host=143.106.241.3;dbname=cl201275;charset=utf8';
+    $db = 'mysql:host=143.106.241.3;dbname=cl201275;charset=utf8'; 
     $user = 'cl201275';
     $pass = 'cl*28092005';
     $pdo = new PDO($db, $user, $pass);
